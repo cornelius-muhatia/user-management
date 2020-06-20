abc4388b4d6bd91443eeac1941fb308afb33cc77
Cornelius Muhatia <muhatiacornelius@gmail.com>
2020-06-20 14:53:43 +0300

    Ignored maker/checker methods on status resource

--- src/main/java/com/cmuhatia/library/library/usermanagement/resources/StatusResource.java
+++ src/main/java/com/cmuhatia/library/library/usermanagement/resources/StatusResource.java
@@ -17,12 +17,20 @@ package com.cmuhatia.library.library.usermanagement.resources;
 
 import com.cm.projects.spring.resource.chasis.ChasisResource;
 import com.cm.projects.spring.resource.chasis.utils.LoggerService;
+import com.cm.projects.spring.resource.chasis.wrappers.ActionWrapper;
+import com.cm.projects.spring.resource.chasis.wrappers.ResponseWrapper;
 import com.cmuhatia.library.library.usermanagement.entities.Status;
 import com.cmuhatia.library.library.usermanagement.entities.TempEditedRecord;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
 import javax.persistence.EntityManager;
+import javax.validation.Valid;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
 
 /**
  * @author Cornelius M.
@@ -30,9 +38,46 @@ import javax.persistence.EntityManager;
  */
 @RestController
 @RequestMapping("/status")
+@ApiOperation(value = "Status Resource", notes = "Used to manage entity statuses")
 public class StatusResource extends ChasisResource<Status, Short, TempEditedRecord> {
 
     public StatusResource(LoggerService loggerService, EntityManager entityManager) {
         super(loggerService, entityManager);
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper<List<String>>> approveActions(@Valid ActionWrapper<Short> actions) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throw new UnsupportedOperationException("Not supported on this resource");
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper> declineActions(@Valid ActionWrapper<Short> actions) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        throw new UnsupportedOperationException("Not supported on this resource");
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper<List<String>>> deactivateRecord(@Valid ActionWrapper<Short> actions) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throw new UnsupportedOperationException("Not supported on this resource");
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper<List<String>>> activateRecord(@Valid ActionWrapper<Short> actions) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throw new UnsupportedOperationException("Not supported on this resource");
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper<List<String>>> lockRecord(@Valid ActionWrapper<Short> actions) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throw new UnsupportedOperationException("Not supported on this resource");
+    }
+
+    @Override
+    @ApiIgnore
+    public ResponseEntity<ResponseWrapper<List<String>>> unLockRecord(@Valid ActionWrapper<Short> actions) throws NoSuchMethodException, InstantiationException, IllegalAccessException, InvocationTargetException {
+        throw new UnsupportedOperationException("Not supported on this resource");
     }
 }
